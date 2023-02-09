72bdc1c6ea8c711bf12ed3da347823e0e37c82ec
Paulo Sabaini <pasabaini@gmail.com>
2023-02-09 13:45:13 -0300

    add section 3 app

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Paulo Antonio Sabaini on 09/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
